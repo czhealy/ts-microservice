09a6830324253cebf92794d750fd6343c40f2a21
czhealy <stantoncz@gmail.com>
2025-01-20 18:50:37 -0700

    Confirmed deployment and api works according to instructions. Some account setup is needed. Cloudwatch role for api was added via AWS Console

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ A Typescript microservice starter project deployed in AWS.
 [SAM CLI](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/install-sam-cli.html)
 
 [NodeJS](https://nodejs.org/en/download)
+
+[NVM](https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating)
+
+## Deploy
+```bash
+sam build -t infrastructure/main.yml
+sam deploy
+```
+* If you don't want to confirm the changeset, set "confirm_changeset" to false
+* Remember to add the Cloudwatch logging role to api gateway, RestApiStage will error with "Logs role ARN must be set in account setting to enable logging" if you forgot.
---
+++ code/api/handlers/thing.ts
@@ -0,0 +1,97 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const listHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify([{
+                name:'ThingOne',
+                status: 'New',
+                code: 'fake',
+                id: '1',
+            }]),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
+
+export const getHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                name:'SingleThing',
+                status: 'New',
+                code: 'sake',
+                id: '2',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
+export const postHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                name:'PostedThing',
+                status: 'New',
+                code: 'rake',
+                id: '3',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
+export const deleteHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                name:'DeletedThing',
+                status: 'Verified',
+                code: 'rake',
+                id: '3',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
