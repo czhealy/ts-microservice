aaf83c8be35f34185295a75dc2febd515e15aca1
czhealy <stantoncz@gmail.com>
2025-01-24 19:18:08 -0700

    Restructuring and adding better documentation

--- README.md
+++ README.md
@@ -10,10 +10,34 @@ A Typescript microservice starter project deployed in AWS.
 
 [NVM](https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating)
 
+[Docker](https://docs.docker.com/desktop/setup/install/mac-install/)
+
+## Unit Test
+
+## Run locally
+```
+sam local start-api
+```
+
 ## Deploy
+Configure AWS Key and secret on your machine or use aws sso cli [instructions](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-sso.html)
+
+Then run
 ```bash
 sam build -t infrastructure/main.yml
 sam deploy
+```
+
+The stack output has your deployed target url.
+![alt text](/docs/images/stackoutput.png)
+
+So, this GET request...
+```
+https://ldlxp3k75a.execute-api.us-east-2.amazonaws.com/v0/things
+```
+Should return
+```
+[{"name":"ThingOne","status":"New","code":"fake","id":"1"}]
 ```
 * If you don't want to confirm the changeset, set "confirm_changeset" to false
 * Remember to add the Cloudwatch logging role to api gateway, RestApiStage will error with "Logs role ARN must be set in account setting to enable logging" if you forgot.
---
+++ code/handlers/queue/deactivation.ts
@@ -0,0 +1,42 @@
+import { Context, EventBridgeEvent, SQSBatchItemFailure, SQSBatchResponse, SQSEvent, SQSHandler, SQSRecord } from "aws-lambda"
+import { Logger } from '@aws-lambda-powertools/logger';
+const logger = new Logger({ serviceName: 'DeactivationCommandHandler' });
+export const commandHandler: SQSHandler = async (event: SQSEvent, _context: Context) => {
+    logger.debug('Received event: %j', { ...event })
+    const recordHandlePromises: Promise<SQSBatchItemFailure | undefined>[] = []
+
+    for (const record of event.Records) {
+        recordHandlePromises.push(handleRecord(record))
+    }
+    const recordHandleResults = await Promise.all(recordHandlePromises)
+    const recordHandleFailures = (await Promise.all(recordHandleResults)).filter(
+        (failure) => failure,
+    )
+    logger.info(
+        'SQS handler batch completed.',
+        {
+            total: recordHandleResults.length,
+            failures: recordHandleFailures.length
+        }
+    )
+    return {
+        batchItemFailures: recordHandleFailures,
+    } as SQSBatchResponse
+}
+async function handleRecord(record: SQSRecord): Promise<SQSBatchItemFailure | undefined> {
+    try {
+        const recordBody = JSON.parse(record.body) as EventBridgeEvent<string, unknown>
+        const command = commandFactory(recordBody.detail as Record<string, unknown>)
+        const provisioned = await provisionService.provision(command)
+        if (provisioned) {
+            logger.info('provision sucessful')
+        } else {
+            logger.warn('provision skipped: %j', command)
+        }
+    } catch (e) {
+        logger.error('RECORD FAILURE=> Error: %s, Record: %j', { error: e, record: record })
+        return {
+            itemIdentifier: record.messageId,
+        }
+    }
+}
